5f00a24388a34f0094bd9c06d97b0c45ba0e6aa7
Caleb Sander <caleb.sander@gmail.com>
2016-03-26 13:16:16 -0400

    Piping and support for sending non-file things

--- fileserver/fileserver.js
+++ fileserver/fileserver.js
@@ -3,61 +3,76 @@ const zlib = require('zlib');
 var mime = require('mime').types;
 
 function isText(mimeType) {
-	return mimeType.startsWith('text/') || mimeType == 'application/json' || mimeType == 'application/javascript';
+	return mimeType.startsWith('text/') || mimeType === 'application/json' || mimeType === 'application/javascript';
 }
-function serv(res, filename, data) {
-	const mimeType = mime[filename.substr(filename.lastIndexOf('.') + 1, filename.length)];
+function serv(res, fileName, statusCode) {
+	servStream(res, mime[fileName.substr(fileName.lastIndexOf('.') + 1, fileName.length)], fs.createReadStream(fileName), statusCode);
+}
+function servStream(res, mimeType, stream, statusCode) {
+	statusCode = statusCode || 200;
 	if (isText(mimeType)) { //only compress text files
 		res.setHeader('Content-Type', mimeType + '; charset=UTF-8');
 		res.setHeader('Content-Encoding', 'gzip');
-		zlib.gzip(data, (err, zipped) => res.end(zipped));
+		res.writeHead(statusCode);
+		stream.pipe(zlib.createGzip()).pipe(res);
 	}
 	else {
 		res.setHeader('Content-Type', mimeType);
-		res.end(data);
+		res.writeHead(statusCode);
+		stream.pipe(res);
 	}
 }
+const ENDING = '.html';
+const DEFAULT_FILE = 'index' + ENDING;
+const FOLDER_TO_DEFAULT_FILE = '/' + DEFAULT_FILE;
 module.exports = function(rootdir, disallow_updir, return404) {
-	if (!return404) {
+	if (return404 === undefined) {
 		return404 = function(res404) {
 			res404.writeHead(404);
 			res404.end();
 		};
 	}
+	else if (return404.constructor === String) {
+		return404 = (function(fileFor404) {
+			return function(res404) {
+				serv(res404, fileFor404, 404);
+			};
+		}(return404));
+	}
 	return function(res, url) {
-		var filename = rootdir + url;
-		if (disallow_updir && url.indexOf('..') != -1) return404(res);
-		else if (url.lastIndexOf('.') != -1) {
-			fs.readFile(filename, function(err, data) {
-				if (err) return404(res);
-				else {
-					serv(res, filename, data);
-					return true;
-				}
-			});
-		}
+		var fileName = rootdir + url;
+		if (disallow_updir && url.indexOf('..') !== -1) return404(res);
 		else {
-			if (filename[filename.length - 1] == '/') {
-				var tryfile = false;
-				filename += 'index.html';
+			if (url.lastIndexOf('.') === -1) {
+				if (fileName[fileName.length - 1] === '/') {
+					var tryFile = false;
+					fileName += DEFAULT_FILE;
+				}
+				else {
+					var tryFile = true;
+					fileName += FOLDER_TO_DEFAULT_FILE;
+				}
+				fs.stat(fileName, function(err, stats) {
+					if (err) {
+						if (tryFile) {
+							fileName = fileName.substr(0, fileName.length - FOLDER_TO_DEFAULT_FILE.length) + ENDING;
+							fs.stat(fileName, function(err, stats) {
+								if (err) return404(res);
+								else serv(res, fileName);
+							});
+						}
+						else return404(res);
+					}
+					else serv(res, fileName);
+				});
 			}
 			else {
-				filename += '/index.html';
-				var tryfile = true;
+				fs.stat(fileName, function(err, stats) {
+					if (err) return404(res);
+					else serv(res, fileName);
+				});
 			}
-			fs.readFile(filename, function(err, data) {
-				if (err) {
-					if (tryfile) {
-						filename = filename.substr(0, filename.length - 11) + '.html';
-						fs.readFile(filename, function(err, data) {
-							if (err) return404(res);
-							else serv(res, filename, data);
-						});
-					}
-					else return404(res);
-				}
-				else serv(res, filename, data);
-			});
 		}
 	}
-}
+};
+module.exports.servStream = servStream;
